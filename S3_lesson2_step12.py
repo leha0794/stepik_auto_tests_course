b817f735842708bf5d6cd7acf6454ef2753622b5
leha0794 <leha0794@mail.ru>
2020-07-21 16:50:31 +0300

    Revert "add tests"
    This reverts commit d4e546c4

--- S3_lesson2_step12.py
+++ S3_lesson2_step12.py
@@ -8,7 +8,6 @@ class TestAbs(unittest.TestCase):
     def test_abs2(self):
         self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
-# Проверка коммита
 
 if __name__ == "__main__":
     unittest.main()
