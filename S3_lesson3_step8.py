45894b6814024ad72f6215b4ddcbfcf032698425
leha0794 <leha0794@mail.ru>
2020-07-22 13:46:53 +0300

    Проходим курс дальше

---
+++ S3_lesson3_step8.py
@@ -0,0 +1,8 @@
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+    # Команда для запуска с подробным отчетом
+    # pytest -v stepik_auto_tests_course/S3_lesson3_step8.py
